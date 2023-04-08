171635c5756dce6d762777792694a92629cf68a6
Shuzhengz <treez.zhang@gmail.com>
2023-04-07 22:16:18 -0700

    more examples and method 2 ini

--- OpenCV/OpenCV_Prototype/method1.py
+++ OpenCV/OpenCV_Prototype/method1.py
@@ -4,9 +4,11 @@ import cv2
 from image_processing import Image_Processing
 from matplotlib import pyplot as plt
 
-img = cv2.imread("image2.jpg")
+img = cv2.imread("image5.jpg")
 
 stop_data = cv2.CascadeClassifier('face_data.xml')
+
+minSize = 50
 
 # generate own haar cascade
 # https://pythonprogramming.net/haar-cascade-object-detection-python-opencv-tutorial/
@@ -15,8 +17,8 @@ test = Image_Processing(img, stop_data)
 
 image = test.get_img_rgb()
 
-if len(test.get_data()) != 0:
-    for (x, y, width, height) in test.get_data():
+if len(test.get_data(minSize)) != 0:
+    for (x, y, width, height) in test.get_data(minSize):
         cv2.rectangle(image, (x, y), (x + height, y + width), (0, 255, 0), 5)
 
 plt.subplot(1, 1, 1)
--- OpenCV/OpenCV_Prototype/method2.py
+++ OpenCV/OpenCV_Prototype/method2.py
@@ -1,1 +1,3 @@
 import cv2
+
+# To be written, depending on if method 1 is enough
